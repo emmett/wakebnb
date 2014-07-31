930e630826153532aac26ec954f7b597d59e9c43
Emmett Faricy <emmett.faricy@gmail.com>
2014-07-31 11:50:01 -0700

    Optimization

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module WAKEbnb
 		config.action_dispatch.default_headers = {
 		  'X-Frame-Options' => 'ALLOWALL'
 		}
+		
+		config.middleware.use Rack::Deflater
+		
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
