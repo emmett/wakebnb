da4371ab32e6cf977066709c3e8f648fc2f478e7
Emmett Faricy <emmett.faricy@gmail.com>
2014-08-01 13:42:35 -0700

    Updated Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,12 @@
 u1 = User.create(username: 'BlackBeard', password: 'password')
 u2 = User.create(username: 'horatio_nelson', password: 'password')
 u3 = User.create(username: 'guest', password: 'password')
+u4 = User.create(username: 'cpt.Ahab', password: 'whitewhale')
 
 u1.create_profile()
 u2.create_profile()
 u3.create_profile()
-
+u4.create_profile()
 
 b1 = u1.boats.create(
 	title: "Flying Dutchman",
@@ -41,11 +42,59 @@ b3 = u3.boats.create(
 	longitude: -120.062,
 	price: 300,
 	boat_photo: File.open('public/seeds/guest_boat.jpg')
-	
 	)
+
+b4 = u3.boats.create(
+	title: "SS Sea Lion",
+	description: "There be sea lions",
+	latitude: 37.830544,
+	longitude:  -122.458714,
+	price: 20,
+	boat_photo: File.open('public/seeds/seals.jpg')
+	)
+b5 = u3.boats.create(
+	title: "USS Refridgerator",
+	description: "Great Gas Mileage",
+	latitude: 37.832544,
+	longitude:  -122.468714,
+	price: 20,
+	boat_photo: File.open('public/seeds/fridge.jpg')
+	)
+b6 = u3.boats.create(
+	title: "SS Sea Lion",
+	description: "There be sea lions",
+	latitude: 39.228,
+	longitude:  -120.06514,
+	price: 20,
+	boat_photo: File.open('public/seeds/boat-sheared.jpg')
+	)
+b7 = u3.boats.create(
+	title: "SS Sea Lion",
+	description: "There be sea lions",
+	latitude: 39.219,
+	longitude:  -120.06514,
+	price: 20,
+	boat_photo: File.open('public/seeds/cabana_boat.jpg')
+	)
+
+b8 = u4.boats.create(
+	title: "Pequod",
+	description: "THAR SHE BLOWS",
+	latitude: 41.358465,
+	longitude: -70.083842,
+	price: 110,
+	boat_photo: File.open('public/seeds/whitewhale.jpg')
+	)
+	
 	
 r1 = u1.reservations.create(
 	boat_id: 2,
+	start_date: Date.new(2014,8,7),
+	end_date: Date.new(2014,8,10)
+	)
+	
+	u2.reservations.create(
+	boat_id: b4.id,
 	start_date: Date.new(2014,8,7),
 	end_date: Date.new(2014,8,10)
 	)
