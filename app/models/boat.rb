21c842c480fc7e00f2dfde93789c1e3e6952f26a
Emmett Faricy <emmett.faricy@gmail.com>
2014-08-13 19:08:53 -0700

    Update gitignore

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -24,39 +24,49 @@ class Boat < ActiveRecord::Base
 	if Rails.env.production?
 		has_attached_file :boat_photo, 
 		:styles  => { :big  => "470X350#" },
+		:storage  =>  :s3,
+		:s3_credentials => {
+			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		},
 		:bucket => ENV['AWS_PROD_BUCKET']
 	else
 		has_attached_file :boat_photo,
+		:storage  =>  :s3,
+		:s3_credentials => {
+			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+		},
 		:bucket => ENV['AWS_DEV_BUCKET']
 	end
 	
 	validates_attachment_content_type(
-		:boat_photo,
-		:content_type => /\Aimage\/.*\Z/ 
+	:boat_photo,
+	:content_type => /\Aimage\/.*\Z/ 
 	)
 	
 	def self.filter(bounds)
 		query_string = <<-SQL
-			SELECT 
-				*
-			FROM 
-				Boats 
-			WHERE 
-				latitude 
-			BETWEEN
-				?
-			AND 
-				? 
-			AND 
-				longitude
-			BETWEEN 
-				? 
-			AND 
-				?
-			SQL
-			filtered_boats = Boat.find_by_sql([query_string, bounds[:minLat], bounds[:maxLat], bounds[:minLng], bounds[:maxLng]])
-			filtered_boats.map{ |boat| puts boat.title }
-			return filtered_boats
+		SELECT 
+		*
+		FROM 
+		Boats 
+		WHERE 
+		latitude 
+		BETWEEN
+		?
+		AND 
+		? 
+		AND 
+		longitude
+		BETWEEN 
+		? 
+		AND 
+		?
+		SQL
+		filtered_boats = Boat.find_by_sql([query_string, bounds[:minLat], bounds[:maxLat], bounds[:minLng], bounds[:maxLng]])
+		filtered_boats.map{ |boat| puts boat.title }
+		return filtered_boats
 	end
 	
 end
